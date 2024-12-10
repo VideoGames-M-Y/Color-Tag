1fce3e06f48f4699b17af4ef535024b487d1643c
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-11 00:49:40 +0200

    Update Singleton.cs

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -3,19 +3,24 @@ using System.Linq;
 
 public class Singleton : MonoBehaviour
 {
-    void Awake()
+    private void Awake()
     {
         string myName = gameObject.name;
-        // The following line is based on code by Isaiah Kelly: http://answers.unity.com/answers/1252385/view.html
-        GameObject[] otherObjectsWithSameName = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == myName).ToArray<GameObject>();
-        Debug.Log("otherObjectsWithSameName.Length: "+otherObjectsWithSameName.Length);
+
+        GameObject[] otherObjectsWithSameName =
+            Resources.FindObjectsOfTypeAll<GameObject>()
+                .Where(obj => obj.name == myName)
+                .ToArray();
+
+        Debug.Log("otherObjectsWithSameName.Length: " + otherObjectsWithSameName.Length);
+
         if (otherObjectsWithSameName.Length > 1)
         {
             Destroy(gameObject);
-        } 
+        }
         else
         {
-            Object.DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject);
         }
     }
 }
