cf8352dadbaabb66f0fb0e402b55ccbf0fa98b42
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-10 23:56:03 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour{
+public class PlayerMovement : MonoBehaviour
+{
 
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 10f;
@@ -9,15 +10,18 @@ public class PlayerMovement : MonoBehaviour{
     [SerializeField] InputAction move = new InputAction(
         type: InputActionType.Value, expectedControlType: nameof(Vector2));
 
-    void OnEnable()  {
+    void OnEnable()
+    {
         move.Enable();
     }
 
-    void OnDisable()  {
+    void OnDisable()
+    {
         move.Disable();
     }
 
-    void Update() {
+    void Update()
+    {
         Vector2 moveDirection = move.ReadValue<Vector2>();
         Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
         transform.position += movementVector;
